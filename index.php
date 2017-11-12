bb6b464fe355e7799c8a77050aefe2e8e2c8406a
Yury <33576679+bar2104y@users.noreply.github.com>
2017-11-12 16:14:55 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,53 @@
 <?php
-$str = '+7979792490 +79194567899        --- .kvhds kfd gsfdh ';
+
+function sendmessege($str){
+	$mes = explode("---", $str);
+	$adresses = explode(" ", $mes[0]);
+	
+	foreach($adresses as $adress){
+		if($adress != ''){
+			$adress = ' '.$adress;
+			if(preg_match("/( \+7| 8)/i", $adress) == 1){
+				#Обработка получателя через телефон
+				#выделить получателя из бд
+				//отправить ему сообщение
+				$men = mysqli_query($connection, "SELECT * FROM `users` WHERE `phone` = '".$adress."'")
+			} else if(preg_match("/@/i", $adress) == 1){
+				# обработка получателя по emai
+			}	
+		}
+	}
+}
+$str = '+7979792490 +79194567899  sdnmfdsbgsb@kbxcvx.ds      --- .kvhds kfd gsfdh ';
 $mes = explode("---", $str);
 var_dump($mes);
-echo '<br>';
+echo '<br><br>';
 
-$adresses = explode(" ", trim($mes[0]));
+$adresses = explode(" ", $mes[0]);
+
+foreach($adresses as $adress){
+	#echo $adress.'<br>';
+	if($adress != ''){
+		$adress = ' '.$adress;
+		if(preg_match("/( \+7| 8)/i", $adress) == 1){
+			echo 'tel '.$adress.'<br>';	
+		} else if(preg_match("/@/i", $adress) == 1){
+			echo 'email '.$adress.'<br>';
+		}
+
+		
+	}
+}
+
+
+
+echo '<br><br>';
+
+$text = trim($mes[1]);
+
+
+
+echo $text.'<br><br>';
 
 var_dump($adresses);
 ?>
